080649c43688b6754ce30f9a1cdddd14069fc7a3
liutengfei <liutengfei@julong.cc>
2017-03-09 18:00:24 +0800

    ddddd

--- spring_mvc_demo2/spring_mvc_zone2/src/main/java/com/julong/ssm/test/Test.java
+++ spring_mvc_demo2/spring_mvc_zone2/src/main/java/com/julong/ssm/test/Test.java
@@ -1,5 +1,13 @@
 package com.julong.ssm.test;
 
+import redis.clients.jedis.Jedis;
+
 public class Test {
+	
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("127.0.0.1",6379);
+		jedis.auth("admin123");
+		System.out.println(jedis.ping());
+	}
 
 }
